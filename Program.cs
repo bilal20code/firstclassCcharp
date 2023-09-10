bdf80105a957bbb550f6c46cf5bd0922e0647e67
Muhammad Bilal Ahmad <111138242+bilal20code@users.noreply.github.com>
2023-09-11 03:29:47 +0500

    firstclassCcharp
    firstclassCcharp

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+      string Text = "Muhammad Bilal Ahmad"; 
+      string phone = "03162640335";
+      int num1 = 5;      
+      int num2 = 6;
+      int x = 5, y = 6, z = 50;  
+      num2 = 13;       
+      double doublenum = 5.99;  
+      char myGrade = 'B';
+      float myFloat = 5.75F;
+      long myLong = 923162640335;         
+      bool Bool = true;   
+                    
+      Console.WriteLine("This is String "+ Text);
+      Console.WriteLine("This is String "+ phone);
+      Console.WriteLine("This is multiple varible" +(x + y + z));
+      Console.WriteLine("This is Plus integer" + (num1 + num2));
+      Console.WriteLine("This is Minus integer" + (num1 - num2));
+      Console.WriteLine("This is Multiplay integer" + (num1 * num2));
+      Console.WriteLine("This is division integer" + (num2 / num1));
+      Console.WriteLine("This is percent sign % integer" + (num1 % 2 ));
+      Console.WriteLine(num1 < num2 ? "This is num1 grather then Varible":"This is num1 less then Varible");
+      Console.WriteLine(num1 > num2 ? "This is num1 grather then Varible":"This is num1 less then Varible");
+      Console.WriteLine("This is Char Varible:" + myGrade);
+      Console.WriteLine("This is Float Varible:" + myFloat);
+      Console.WriteLine("This is Long Varible:" + myLong);  
+      Console.WriteLine("This is String "+ doublenum);
+      Console.WriteLine("This is Booleans varible "+ Bool);
+
+       
